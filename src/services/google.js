7d3360751c41f247bf1680e6f5510b92480b51ea
ethyashpathak <ethyashpathak@gmail.com>
2026-05-28 14:24:48 +0530

    changed the callback route because of new deployment

--- src/services/google.js
+++ src/services/google.js
@@ -3,7 +3,7 @@ const { google } = require("googleapis");
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  "https://donna-backend-0lji.onrender.com/auth/google/callback"
+  "https://donna-backend-htft.onrender.com/auth/google/callback"
 );
 
 module.exports = oauth2Client;
